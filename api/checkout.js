cd4a4c0ef6b71ae722b27d07f6d384484ac4ba41
valginer0 <valginer0@gmail.com>
2026-02-22 03:55:55 -0600

    fix: change checkout mode to subscription for recurring billing

--- api/checkout.js
+++ api/checkout.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
     }
 
     const session = await stripe.checkout.sessions.create({
-      mode: 'payment',
+      mode: 'subscription',
       payment_method_types: ['card'],
       line_items: [
         {
